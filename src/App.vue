325a2fb145eab18e5c0f72402c9d91db8614bddc
yh-il002 <y.hiramatsu1122@gmail.com>
2025-07-31 09:45:54 +0900

    検索結果のスタイルを適用

--- src/App.vue
+++ src/App.vue
@@ -23,19 +23,25 @@ const filteredData = computed(() => {
 </script>
 
 <template>
-  <h1>桃鈴ねね YouTube動画 アーカイブ</h1>
-  <div>
-    <input v-model="keyword" placeholder="キーワードで検索" />
-    <label>開始日：<input type="date" v-model="startDate" /></label>
-    <label>終了日：<input type="date" v-model="endDate" /></label>
-  </div>
-  <div class="item-list-wrap">
-    <div v-for="item in filteredData" :key="item.title" class="item-list">
-      <a :href="item.youtube_url" class="item-list-link" target="_blank">
-        <img :src="item.thumbnail_url" alt="" class="item-list-thumbnail">
-        <h2 class="item-list-title">{{ item.title }}</h2>
-        <p class="item-list-date">{{ item.release_date }}</p>
-      </a>
+  <div class="wapper">
+    <h1>桃鈴ねね YouTube動画 アーカイブ</h1>
+    <div class="serch-warp">
+      <input v-model="keyword" placeholder="キーワードで検索" class="input-keyword" />
+      <input type="date" v-model="startDate" class="input-startdate" />
+      <span>〜</span>
+      <input type="date" v-model="endDate" class="input-enddate" />
+    </div>
+    <div class="serch-num">検索結果：{{ filteredData.length }}件</div>
+    <div class="item-list-wrap">
+      <div v-for="item in filteredData" :key="item.title" class="item-list">
+        <a :href="item.youtube_url" class="item-list-link" target="_blank">
+          <img :src="item.thumbnail_url" alt="" class="item-list-thumbnail">
+          <div>
+            <h2 class="item-list-title">{{ item.title }}</h2>
+            <p class="item-list-date">{{ item.release_date }}</p>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
   <!-- 
@@ -51,8 +57,8 @@ const filteredData = computed(() => {
    -->
 </template>
 
-<style scoped>
-.logo {
+<style lang="scss" scoped>
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -63,5 +69,77 @@ const filteredData = computed(() => {
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+} */
+
+.wapper {
+  width: 960px;
+  margin: 0 auto;
+
+  h1 {
+    font-size: 3.4rem;
+  }
 }
+
+.serch-warp {
+  margin: 24px 0;
+
+  span {
+    font-size: 1.8rem;
+    padding: 0 12px;
+  }
+}
+
+.input-keyword {
+  width: 300px;
+  height: 40px;
+  font-size: 1.8rem;
+}
+
+.input-startdate {
+  width: 150px;
+  height: 40px;
+  font-size: 1.8rem;
+  margin-left: 24px;
+}
+
+.input-enddate {
+  width: 150px;
+  height: 40px;
+  font-size: 1.8rem;
+}
+
+.serch-num {
+  font-size: 1.8rem;
+}
+
+.item-list {
+  border-radius: 20px;
+
+  &:hover {
+    background-color: #ffb65d;
+    transition: all 0.1s ease-in;
+  }
+}
+
+.item-list-link {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  column-gap: 16px;
+  padding: 18px;
+  color: #333;
+}
+
+.item-list-thumbnail {
+  border-radius: 12px;
+}
+
+.item-list-title {
+  font-size: 1.8rem;
+}
+
+.item-list-date {
+  font-size: 1.4rem;
+  margin-top: 8px;
+}
+
 </style>
