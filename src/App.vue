940af57f6911a496899e48c3ed08fd8190fb43b9
yh-il002 <y.hiramatsu1122@gmail.com>
2025-08-05 19:31:27 +0900

    navのスタイル調整

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">桃鈴ねねYouTube動画検索</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/hologura">ホロぐら検索</router-link>
+      <ul>
+        <li><router-link to="/">桃鈴ねね</router-link></li>
+        <li><router-link to="/hologura">ホロぐら</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
     </nav>
-    <p>↑↑↑↑↑ページ切り替え↑↑↑↑↑</p>
     <router-view />
   </div>
 </template>
@@ -15,13 +14,45 @@
 <script setup lang="ts">
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@use './assets/scss/mq' as *;
+
 nav {
   margin-bottom: 1rem;
   font-size: 2.4rem;
-}
+  margin: 24px auto;
 
-p {
-  font-size: 1.8rem;
+  ul {
+    list-style: none;
+    width: 960px;
+    margin: 0 auto;
+    padding: 12px 0;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    background-color: #27c7ff;
+    border-radius: 50px;
+
+    @include mq($until: desktop) {
+      width: 90vw;
+    }
+  }
+
+  li {
+    font-size: 2.2rem;
+    text-align: center;
+
+    @include mq($until: desktop) {
+      font-size: 1.4rem;
+    }
+
+    &:hover {
+      opacity: 0.5;
+    }
+    
+    a {
+      color: #fff;
+      font-weight: bold;
+    }
+  }
 }
 </style>
