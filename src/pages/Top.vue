78534c0542e682329d39f18bf30c76cc2bc4c2bd
yh-il002 <y.hiramatsu1122@gmail.com>
2025-08-07 22:30:49 +0900

    Google Sans Codeフォントを追加、TOPページcss調整

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,15 +1,28 @@
 <template>
-  <h1>このサイトは？</h1>
-  <p>ホロライブ所属タレントさんの過去のYouTube動画を検索できるサイトです。<br>YouTube上で ”歌枠” 等で検索をした時に、過去のもの全てが出てこないことがあり一覧で全てを見れる仕組みがほしかったので、自分のために作りました。<br>今後は検索できるタレントさんを増やしていく予定です。<br>自分用に作ったサイトですが、ホロライブリスナーの皆様にも活用していただけたら嬉しいです。</p>
-  <a href="https://x.com/house_mannequin" target="_blank">作者：ハウスマヌカン🍑🥟HouseMannequin（Twitter）</a>
+  <div class="wrapper">
+    <h1>このサイトは？</h1>
+    <p>ホロライブ所属タレントさんの過去のYouTube動画を検索できるサイトです。<br>YouTube上で ”歌枠” 等で検索をした時に、過去のもの全てが出てこないことがあり、一覧で全てを見れる仕組みがほしかったので自分のために作りました。<br>今後は検索できるタレントさんを増やしていく予定です。<br>自分用に作ったサイトですが、ホロライブリスナーの皆様にも活用していただけたら嬉しいです。</p>
+    <a href="https://x.com/house_mannequin" target="_blank">作者：ハウスマヌカン🍑🥟HouseMannequin（Twitter）</a>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/mq' as *;
 
+.wrapper {
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 40px;
+
+  @include mq($until: desktop) {
+    width: 100%;
+    padding-top: 10px;
+  }
+}
+
 h1 {
   font-size: 2.8rem;
-  margin-bottom: 18px;
+  margin-bottom: 24px;
 }
 
 p {
@@ -19,6 +32,8 @@ p {
 
 a {
   font-size: 2rem;
+  width: 80%;
+  display: block;
 }
 
 </style>
